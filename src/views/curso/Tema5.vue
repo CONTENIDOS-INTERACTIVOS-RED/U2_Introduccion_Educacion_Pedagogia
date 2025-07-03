3e704d048e685dc0a48e4d493a79817d8a5aa8be
Francisco Lizcano <flizcanor@icloud.com>
2025-07-02 21:39:50 -0500

    se vincula sintesis

---
+++ src/views/curso/Tema5.vue
@@ -0,0 +1,8 @@
+<template lang="pug">
+  .curso-main-container.creditos-vista
+    BannerInterno.text-uppercase(:titulo="'unidad 2: Teorías pedagógicas y su impacto en la educación'" :subTitulo="'SÍNTESIS'")
+    .container.tarjeta.tarjeta--blanca.p-4.p-md-5
+      p.mb-4.mb-md-5 La Unidad 2: Teorías pedagógicas y su impacto en la educación, explora los fundamentos teóricos esenciales de las principales corrientes pedagógicas y su influencia en el desarrollo de prácticas educativas efectivas. Esta unidad profundiza en el conductismo, cognitivismo y constructivismo, abordando su evolución histórica, principales exponentes y aplicaciones en el aula. Además, examina el rol del docente en cada una de estas teorías, destacando cómo cada enfoque determina su función como mediador, facilitador y guía en el proceso de aprendizaje. La comprensión de estas teorías proporciona a los estudiantes un marco analítico para reflexionar sobre la educación, permitiéndoles analizar críticamente sus prácticas y mejorar la calidad de la enseñanza en diversos contextos educativos.
+      figure
+        img(src='@/assets/curso/sintesis.svg', alt='Texto que describa la imagen')
+</template>
